ac08731a7a3ebada4f3de3c0fae776f6bded77b5
mrcv20 <marcos.valentee20@gmail.com>
2021-09-06 21:34:45 -0400

    routes for classes, comments and users

--- src/routes.js
+++ src/routes.js
@@ -2,23 +2,31 @@ import { Router} from 'express'
 
 import UserController from './app/controllers/UserController'
 import ClasseController from './app/controllers/ClasseController'
-import CommentController from './app/controllers/CommentController'
+import authMiddleware from './app/middlewares/auth'
 
 const routes = Router()
 
+routes.use(authMiddleware)
 
 // USERS ROUTES
 routes.get('/users', UserController.allUsers)
-routes.post('/users', UserController.create)
+routes.post('/auth/signup', UserController.signup)
+routes.post('/auth/login', UserController.login)
+
+
+// COMMENTS ROUTES
+routes.get('/classes/comments', ClasseController.allComments)
+routes.post('/classes/comments', ClasseController.create_comment)
+routes.delete('/classes/comments/:commentId', ClasseController.delete_comment)
 
 // CLASSES ROUTES
 routes.get('/classes', ClasseController.allClasses)
+routes.get('/classes/:classeId', ClasseController.getOneClasse)
 routes.post('/classes', ClasseController.create)
 routes.delete('/classes/:classeId', ClasseController.delete)
 
-// COMMENTS ROUTES
-routes.get('/classes/comments', CommentController.allComments)
-routes.post('/classes/comments', CommentController.create)
+
+
 
 
 export default routes
