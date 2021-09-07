2b59a755d76881604b4e71e13765537689dd4193
mrcv20 <marcos.valentee20@gmail.com>
2021-09-06 21:37:41 -0400

    controllers for classes and comments

--- src/app/controllers/ClasseController.ts
+++ src/app/controllers/ClasseController.ts
@@ -1,25 +1,88 @@
 import { Request, Response } from 'express'
 
 import Classe from '../schemas/Classe'
+import Comment from '../schemas/Comment'
 
 class ClasseController {
-    public async allClasses (req: Request, res: Response): Promise<Response> {
-        const classes = await Classe.find()
+    public async allComments (req: Request, res: Response): Promise<Response> {
+        try {
 
-        return res.json(classes)
+            const comments = await Comment.find().populate('id_class')
+    
+            return res.send({ comments })
+            
+        } catch (err) {
+            console.log(err)
+            return res.status(400).send({ error: "Error loading comments"})
+        }
+    }
+
+    public async create_comment (req: Request, res: Response): Promise<Response> {
+        try {
+            const comment = await Comment.create(req.body)
+    
+            return res.send({ comment })
+    
+        } catch(err){
+            return res.status(400).send({ error: "Error creating new comment "})
+        }
+    }
+    public async delete_comment (req: Request, res: Response): Promise<Response> {
+        try {
+            await Comment.findByIdAndRemove(req.params.commentId)
+        
+            return res.send({ msg: "deleted comment " })
+        } catch (err) {
+            return res.status(400).send({ error: "error loading classe"})
+        }
+    }
+
+
+    public async allClasses (req: Request, res: Response): Promise<Response> {
+        try {
+            const classes = await Classe.find()
+    
+            return res.send({ classes })
+    
+        } catch (err) {
+            console.log(err)
+            return res.status(400).send({ error: "Error loading classes"})
+        }
     }
 
     public async create (req: Request, res: Response): Promise<Response> {
-        const classe = await Classe.create(req.body)
-
-        return res.json(classe)
+        try {
+            const {name, description, video} = req.body
+    
+            const classe = await Classe.create({name, description, video})
+    
+            return res.send({ classe })
+    
+        } catch(err){
+            return res.status(400).send({ error: "Error creating new classe"})
+        }
     }
 
     public async delete (req: Request, res: Response): Promise<Response> {
-        await Classe.findByIdAndRemove(req.params.classeId)
-
-        return res.json({classe: "deleted"})
+        try {
+            await Classe.findByIdAndRemove(req.params.classeId)
+        
+            return res.send({ msg: "deleted" })
+        } catch (err) {
+            return res.status(400).send({ error: "error loading classe"})
+        }
     }
+
+    public async getOneClasse (req: Request, res: Response): Promise<Response> {
+        try {
+            const user = await Classe.findByIdAndRemove(req.params.classeId)
+        
+            return res.send({ user })
+        } catch (err) {
+            return res.status(400).send({ error: "error loading classe"})
+        }
+    }
+
 }
 
 export default new ClasseController()
