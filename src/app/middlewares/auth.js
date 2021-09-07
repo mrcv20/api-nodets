55efb55014a1d8933173a6544b03e94812181cdc
mrcv20 <marcos.valentee20@gmail.com>
2021-09-06 21:33:36 -0400

    middleware to validate token checks

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,35 @@
+import * as jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth.json'
+
+module.exports = (req, res, next) => {
+    // buscando header da request
+    const authHeader = req.headers.authorization
+    
+
+
+    // verifying with conditionals is soft than process the token direct in the jwt
+    if (!authHeader)
+        return res.status(401).send({ error: 'no token provided'})
+
+    const parts = authHeader.split(' ')
+
+    // Bearer qpoewikpqweokopkqwepkqwp
+    if (!parts.length == 2 )
+        return restz.status(401).send({ error: 'token error'})
+
+    const [ scheme, token ] = parts
+
+    //      Bearer {token }
+
+    // regex schemes for Bearer
+    if (!/^Bearer$/i.test(scheme))
+        return res.status(401).send({ error: 'token not formatted'})
+
+    // last veryfing
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) return res.status(401).send({ error: 'token not valid'})
+
+        req.userId = decoded.id
+        return next()
+    })
+}
